3dc328c09b6fbab6379bcc15850f416429bc65f2
Saikiran <saikiranmohanty@gmail.com>
2021-12-12 20:18:21 +0530

    first commit

---
+++ map/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $(".hide").hide();
+    
+    
+    $(".marker-russia").click(function(){
+      $(".hide").hide();
+      $(".russia-txt").fadeIn(300);
+    });
+
+    $(".marker-india").click(function(){
+      $(".hide").hide();
+      $(".india-txt").fadeIn(300);
+    });
+    
+      $(".marker-usa").click(function(){
+         $(".hide").hide();
+      $(".usa-txt").fadeIn(300);
+    });
+    
+      $(".marker-uk").click(function(){
+         $(".hide").hide();
+      $(".uk-txt").fadeIn(300);
+    });
+    
+      $(".marker-brazil").click(function(){
+         $(".hide").hide();
+      $(".brazil-txt").fadeIn(300);
+    });
+  
+  });
